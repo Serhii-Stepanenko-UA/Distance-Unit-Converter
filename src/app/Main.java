99e336563b260ddf09805a57bedbb5329d44255e
Pulsar UA <sergivstep@gmail.com>
2024-06-08 14:02:13 +0300

    Add mls to kms convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,18 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_K = 1.609344;
+
     public static void main(String[] args) {
         System.out.println("Converter App.");
+
+        double mi = 5;
+        double kms = convMlsToKms(mi);
+        System.out.println("Result is " + kms + " kilometers.");
+    }
+
+    private static double convMlsToKms(double mi) {
+        return mi / CONV_K;
     }
 }
